aad49c707665b4ad56f32b3166ec545968780adb
Elisée Maurer <elisee@sparklinlabs.com>
2016-01-18 16:13:25 +0100

    Add tray icon to run app in background, allow opening local server in new tab

--- src/node/main.ts
+++ src/node/main.ts
@@ -3,22 +3,52 @@
 import * as electron from "electron";
 
 let mainWindow: GitHubElectron.BrowserWindow;
+let appIcon: GitHubElectron.Tray;
+let appIconMenu: GitHubElectron.Menu;
 
-electron.app.on("window-all-closed", function() {
-  if (process.platform !== "darwin") electron.app.quit();
-});
+let quit = false;
+
+electron.app.on("window-all-closed", () => { /* Nothing */ });
 
 electron.app.on("ready", function() {
   electron.Menu.setApplicationMenu(null);
+  setupAppIcon();
+  setupMainWindow();
+});
+
+function setupAppIcon() {
+  appIcon = new electron.Tray(`${__dirname}/icon-16.png`);
+  appIcon.setToolTip("Superpowers");
+  appIcon.on("double-click", () => { mainWindow.show(); });
+  appIconMenu = electron.Menu.buildFromTemplate([
+    { type: "separator" },
+    { label: "Dashboard", type: "normal", click: () => { mainWindow.show(); } },
+    { type: "separator" },
+    { label: "Exit", type: "normal", click: () => { quit = true; electron.app.quit(); } }
+  ]);
+  appIcon.setContextMenu(appIconMenu);
+
+  // TODO: Insert 5 most recently used servers
+  appIconMenu.insert(0, new electron.MenuItem({ label: "My Server", type: "normal", click: () => {} }));
+}
+
+function setupMainWindow() {
   mainWindow = new electron.BrowserWindow({
     width: 1000, height: 600,
     useContentSize: true, autoHideMenuBar: true,
     show: false
   });
   mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+
   mainWindow.webContents.on("did-finish-load", () => {
-    // mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
     mainWindow.show();
   });
-});
-  
+
+  mainWindow.on("close", (event) => {
+    if (quit) return;
+
+    event.preventDefault();
+    mainWindow.hide();
+  });
+}
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -4,7 +4,9 @@ import * as TreeView from "dnd-tree-view";
 import * as TabStrip from "tab-strip";
 
 const sidebarElt = document.querySelector(".sidebar");
-
+const tabsBarElt = document.querySelector(".tabs-bar") as HTMLElement;
+const tabStrip = new TabStrip(tabsBarElt);
+const panesElt = document.querySelector(".panes");
 
 // Servers tree view
 const serversTreeView = new TreeView(document.querySelector(".servers-tree-view") as HTMLElement, { dropCallback: onServerDrop });
@@ -13,6 +15,7 @@ serversTreeView.on("activate", onServerActivate);
 
 const myServerElt = document.createElement("li");
 myServerElt.textContent = "My Server";
+myServerElt.dataset["serverId"] = "0";
 serversTreeView.append(myServerElt, "item");
 
 function onServerDrop(dropInfo: { target: HTMLLIElement; where: string; }, orderedNodes: HTMLLIElement[]) {
@@ -21,37 +24,84 @@ function onServerDrop(dropInfo: { target: HTMLLIElement; where: string; }, order
 }
 
 function updateSelectedServer() {
-  // TODO
+  if (serversTreeView.selectedNodes.length === 0) {
+    // TODO
+  } else {
+    // TODO
+  }
 }
 
 function onServerActivate() {
-  // TODO
+  if (serversTreeView.selectedNodes.length === 0) return;
+
+  let serverId = serversTreeView.selectedNodes[0].dataset["serverId"];
+  openServer(serverId);
+}
+
+function openServer(serverId: string) {
+  const activeTabElt = tabStrip.tabsRoot.querySelector("li.active") as HTMLLIElement;
+  if (activeTabElt != null) {
+    activeTabElt.classList.remove("active");
+    (panesElt.querySelector("iframe.active") as HTMLIFrameElement).classList.remove("active");
+  }
+
+  let serverTabElt = tabsBarElt.querySelector(`li[data-server-id="${serverId}"]`) as HTMLLIElement;
+  let serverIframeElt = panesElt.querySelector(`iframe[data-server-id="${serverId}"]`) as HTMLIFrameElement;
+
+  if (serverTabElt == null) {
+    serverTabElt = document.createElement("li");
+    serverTabElt.dataset["serverId"] = serverId;
+    serverTabElt.textContent = "My Server";
+    tabStrip.tabsRoot.appendChild(serverTabElt);
+
+    serverIframeElt = document.createElement("iframe");
+    serverIframeElt.dataset["serverId"] = serverId;
+    serverIframeElt.src = "http://localhost:8000/";
+    panesElt.appendChild(serverIframeElt);
+  }
+
+  serverTabElt.classList.add("active");
+  serverIframeElt.classList.add("active");
 }
 
 // Tab strip
-const tabsBarElt = document.querySelector(".tabs-bar") as HTMLElement;
-const tabStrip = new TabStrip(tabsBarElt);
 tabStrip.on("activateTab", onTabActivate);
 tabStrip.on("closeTab", onTabClose);
 
-const homeTabElt = document.createElement("li");
-homeTabElt.textContent = "H";
-homeTabElt.classList.add("active");
-homeTabElt.classList.add("pinned");
+function onTabActivate(serverTabElt: HTMLLIElement) {
+  const activeTab = tabStrip.tabsRoot.querySelector(".active");
+  if (activeTab != null) {
+    activeTab.classList.remove("active");
+    (panesElt.querySelector("iframe.active") as HTMLIFrameElement).classList.remove("active");
+  }
 
-tabStrip.tabsRoot.appendChild(homeTabElt);
+  serverTabElt.classList.add("active");
+  const serverId = serverTabElt.dataset["serverId"];
+  const toolName = serverTabElt.dataset["name"];
 
-function onTabActivate(tabElement: HTMLLIElement) {
-  // TODO
+  if (serverId != null) {
+    const serverIframeElt = panesElt.querySelector(`iframe[data-server-id="${serverId}"]`) as HTMLIFrameElement;
+    serverIframeElt.classList.add("active");
+  } else {
+    const toolIframeElt = panesElt.querySelector(`iframe[data-name="${toolName}"]`) as HTMLIFrameElement;
+    toolIframeElt.classList.add("active");
+  }
 }
 
 function onTabClose(tabElement: HTMLLIElement) {
   // TODO
 }
 
-// Panes
-const panesElt = document.querySelector(".panes");
+// Home tab
+const homeTabElt = document.createElement("li");
+homeTabElt.textContent = "H";
+homeTabElt.dataset["name"] = "home";
+homeTabElt.classList.add("active");
+homeTabElt.classList.add("pinned");
+tabStrip.tabsRoot.appendChild(homeTabElt);
 
 const homeIframeElt = document.createElement("iframe");
 homeIframeElt.src = "http://superpowers-html5.com/";
+homeIframeElt.classList.add("active");
+homeIframeElt.dataset["name"] = "home";
 panesElt.appendChild(homeIframeElt);
