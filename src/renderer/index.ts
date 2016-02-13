7b195e92ad146bab7e81370af67fcf3254aeb6f5
bilou84 <nicolas.gauthier84@gmail.com>
2016-02-13 23:57:42 +0100

    WIP auto-install of core and game system

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -6,6 +6,7 @@ import * as splashScreen from "./splashScreen";
 import * as updateManager from "./updateManager";
 import * as sidebar from "./sidebar";
 import * as home from "./home";
+import * as childProcess from "child_process";
 
 setupDevToolsShortcut();
 
@@ -63,4 +64,24 @@ function start() {
   home.start();
 
   splashScreen.fadeOut();
+
+  // TEMPORARY: automatically start the server
+  const serverPath = `${settings.userDataPath}/core/server/index.js`;
+
+  const serverEnv: { [key: string]: string; } = {};
+  serverEnv["ELECTRON_RUN_AS_NODE"] = "1";
+  serverEnv["ELECTRON_NO_ATTACH_CONSOLE"] = "1";
+
+  // NOTE: It would be nice to simply copy all environment variables
+  // but somehow, this prevents Electron 0.35.1 from starting the server
+  // for (const key in nodeProcess.env) serverEnv[key] = nodeProcess.env[key];
+
+  // So instead, we'll just copy the environment variables we definitely need
+  if (process.env["NODE_ENV"] != null) serverEnv["NODE_ENV"] = process.env["NODE_ENV"];
+  if (process.env["APPDATA"] != null) serverEnv["APPDATA"] = process.env["APPDATA"];
+  if (process.env["HOME"] != null) serverEnv["HOME"] = process.env["HOME"];
+  if (process.env["XDG_DATA_HOME"] != null) serverEnv["XDG_DATA_HOME"] = process.env["XDG_DATA_HOME"];
+
+  const serverProcess = childProcess.fork(serverPath, ["start", `--data-path=${settings.userDataPath}`], { silent: true, env: serverEnv });
+  serverProcess.on("message", (msg: string) => { console.log(msg); });
 }
--- src/renderer/updateManager.ts
+++ src/renderer/updateManager.ts
@@ -2,10 +2,17 @@ import * as electron from "electron";
 import * as async from "async";
 import * as fs from "fs";
 import * as dialogs from "simple-dialogs";
+import * as mkdirp from "mkdirp";
+import * as dummy_https from "https";
 import * as settings from "./settings";
 import * as i18n from "../shared/i18n";
 import * as splashScreen from "./splashScreen";
 import fetch from "../shared/fetch";
+
+/* tslint:disable */
+const https: typeof dummy_https = require("follow-redirects").https;
+const unzip = require("unzip");
+/* tslint:enable */
 
 export let appVersion = electron.remote.app.getVersion();
 if (appVersion === "0.0.0-dev") appVersion = `v${require(`${__dirname}/../../package.json`).version}-dev`;
@@ -15,11 +22,13 @@ interface ComponentInfo {
   repository: string;
   current: string;
   latest: string;
+  downloadURL?: string;
 }
 
 const components: { [name: string]: ComponentInfo } = {
   "app": { repository: "superpowers/superpowers-app", current: appVersion, latest: null },
-  "core": { repository: "superpowers/superpowers-core", current: null, latest: null }
+  "core": { repository: "superpowers/superpowers-core", current: null, latest: null },
+  "game": { repository: "superpowers/superpowers-game", current: null, latest: null }
 };
 
 function fetchVersions(callback: (err: Error) => void) {
@@ -30,22 +39,51 @@ function fetchVersions(callback: (err: Error) => void) {
 
     if (corePackageJSON != null) {
       const corePackage = JSON.parse(corePackageJSON);
-      components["core"].current = corePackage.version;
+      components["core"].current = `v${corePackage.version}`;
     }
 
     async.each(Object.keys(components), (name, cb) => {
       fetch(`https://api.github.com/repos/${components[name].repository}/releases/latest`, "json", (err, lastRelease) => {
         if (err != null) { cb(err); return; }
         components[name].latest = lastRelease.tag_name as string;
+        components[name].downloadURL = lastRelease.assets[0].browser_download_url as string;
         cb();
       });
     }, callback);
   });
 }
 
+function downloadRelease(downloadURL: string, downloadPath: string, callback: (err: string) => void) {
+  mkdirp.sync(downloadPath);
+  https.get({
+    hostname: "github.com",
+    path: downloadURL,
+    headers: { "user-agent": "Superpowers" }
+  }, (res) => {
+    if (res.statusCode !== 200) {
+      callback(`Unexpected status code: ${res.statusCode}`);
+      return;
+    }
+
+    let rootFolderName: string;
+    res.pipe(unzip.Parse())
+      .on("entry", (entry: any) => {
+        if (rootFolderName == null) {
+          rootFolderName = entry.path;
+          return;
+        }
+
+        const entryPath = `${downloadPath}/${entry.path.replace(rootFolderName, "")}`;
+        if (entry.type === "Directory") mkdirp.sync(entryPath);
+        else entry.pipe(fs.createWriteStream(entryPath));
+      })
+      .on("close", () => { callback(null); });
+  });
+}
+
 export function checkForUpdates(callback: Function) {
   // TODO: Offer installing a system on first run!
-  async.series([ fetchVersions, checkAppUpdate, checkCoreUpdate ],
+  async.series([ fetchVersions, checkAppUpdate, checkUpdates ],
   (err) => { callback(); });
 }
 
@@ -71,14 +109,42 @@ function checkAppUpdate(callback: Function) {
   });
 }
 
-function checkCoreUpdate(callback: Function) {
+function checkUpdates(callback: Function) {
   const core = components["core"];
-  if (core.latest == null || core.latest === core.current) { callback(); return; }
+  if (core.latest == null) return;
 
+  // First installation
   if (core.current == null) {
-    installCore(callback);
+    installCore((err) => {
+      if (err != null) return;
+
+      const label = i18n.t("startup:updateAvailable.askGameInstall");
+      const options = {
+        validationLabel: i18n.t("common:actions.install"),
+        cancelLabel: i18n.t("common:actions.skip")
+      };
+      new dialogs.ConfirmDialog(label, options, (installGame) => {
+        if (installGame) {
+          splashScreen.setStatus(i18n.t("startup:status.installingGame"));
+          // FIXME: Use common stuff from the core folder
+          downloadRelease(components["game"].downloadURL, `${settings.userDataPath}/systems/game`, (error) => {
+            if (error != null) {
+              new dialogs.InfoDialog(i18n.t("startup:status.installingGameFailed", { error }), null, () => {
+                callback(error);
+              });
+            } else {
+              callback(null);
+            }
+          });
+          return;
+        }
+        callback();
+      });
+    });
     return;
   }
+
+  if (core.latest === core.current) { callback(null); return; }
 
   const label = i18n.t("startup:updateAvailable.core", { latest: core.latest, current: core.current });
   const options = {
@@ -87,16 +153,28 @@ function checkCoreUpdate(callback: Function) {
   };
 
   new dialogs.ConfirmDialog(label, options, (shouldUpdate) => {
-    if (shouldUpdate) { installCore(callback); return; }
+    if (shouldUpdate) {
+      // FIXME: Use common stuff from the core folder
+      installCore(() => { callback(); });
+      return;
+    }
     callback();
   });
 
   return;
 }
 
-function installCore(callback: Function) {
+function installCore(callback: (error: string) => void) {
   splashScreen.setStatus(i18n.t("startup:status.installingCore"));
 
-  // TODO: Actually install core
-  setTimeout(callback, 1000);
+  downloadRelease(components["core"].downloadURL, `${settings.userDataPath}/core`, (error) => {
+    if (error != null) {
+      new dialogs.InfoDialog(i18n.t("startup:status.installingCoreFailed", { error }), null, () => {
+        callback(error);
+      });
+    } else {
+      splashScreen.setStatus(i18n.t("startup:status.installingCoreSucceed"));
+      callback(null);
+    }
+  });
 }
