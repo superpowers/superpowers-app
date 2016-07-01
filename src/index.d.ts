d4ffe9594631cd1354f3f8dcb40755590795d874
Elisée Maurer <elisee@sparklinlabs.com>
2016-07-01 03:21:19 +0200

    Switch back to tsd

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/main.d.ts" />
+/// <reference path="../typings/tsd.d.ts" />
 
 interface ServerEntry {
   id: string;
---
+++ typings/tsd.d.ts
@@ -0,0 +1,10 @@
+
+/// <reference path="node/node.d.ts" />
+/// <reference path="async/async.d.ts" />
+/// <reference path="github-electron/github-electron.d.ts" />
+/// <reference path="electron-packager/electron-packager.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="mkdirp/mkdirp.d.ts" />
+/// <reference path="slate-irc/slate-irc.d.ts" />
+/// <reference path="yargs/yargs.d.ts" />
+/// <reference path="escape-html/escape-html.d.ts" />
