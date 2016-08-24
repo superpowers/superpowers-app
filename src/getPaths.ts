07210b41867ee666c2c6a58754c3c15d40786fc0
Elisée Maurer <elisee@sparklinlabs.com>
2016-08-24 07:43:36 +0200

    Use electron.app.getPath("appData") instead of custom logic to store user data
    We'll try to migrate from old data path to new one if new one doesn't exist

--- src/getPaths.ts
+++ src/getPaths.ts
@@ -1,3 +1,4 @@
+import * as electron from "electron";
 import * as yargs from "yargs";
 import * as path from "path";
 import * as fs from "fs";
@@ -19,24 +20,36 @@ export default function getPaths(callback: (err: LocalizedError, corePath?: stri
     return;
   }
 
-  switch (process.platform) {
-    case "win32":
-      if (process.env.APPDATA != null) dataPath = path.join(process.env.APPDATA, "Superpowers");
-      else error = new LocalizedError("startup:errors.missingEnvironmentVariables", { envVars: "APPDATA" });
-      break;
-    case "darwin":
-      if (process.env.HOME != null) dataPath = path.join(process.env.HOME, "Library", "Superpowers");
-      else error = new LocalizedError("startup:errors.missingEnvironmentVariables", { envVars: "HOME" });
-      break;
-    default:
-      if (process.env.XDG_DATA_HOME != null) dataPath = path.join(process.env.XDG_DATA_HOME, "Superpowers");
-      else if (process.env.HOME != null) dataPath = path.join(process.env.HOME, ".local/share", "Superpowers");
-      else error = new LocalizedError("startup:errors.missingEnvironmentVariables", { envVars: "XDG_DATA_HOME, HOME" });
+  try {
+    dataPath = path.join(electron.app.getPath("appData"), "Superpowers");
+  } catch (err) {
+    process.nextTick(() => { callback(new LocalizedError("startup:errors.couldNotGetDataPath", { details: err.message })); });
+    return;
   }
 
-  if (error != null) {
-    process.nextTick(() => { callback(error); });
-    return;
+  console.log(dataPath);
+
+  if (!fs.existsSync(dataPath)) {
+    // This is the old custom logic we used to determine the appData folder
+    // so if the new data folder doesn't exist, we'll try to migrate from the old one
+    let oldDataPath: string;
+
+    switch (process.platform) {
+      case "win32":
+        if (process.env.APPDATA != null) oldDataPath = path.join(process.env.APPDATA, "Superpowers");
+        break;
+      case "darwin":
+        if (process.env.HOME != null) oldDataPath = path.join(process.env.HOME, "Library", "Superpowers");
+        break;
+      default:
+        if (process.env.XDG_DATA_HOME != null) oldDataPath = path.join(process.env.XDG_DATA_HOME, "Superpowers");
+        else if (process.env.HOME != null) oldDataPath = path.join(process.env.HOME, ".local/share", "Superpowers");
+    }
+
+    if (oldDataPath != null && fs.existsSync(oldDataPath)) {
+      console.log(`Migrating data from ${oldDataPath} to ${dataPath}...`);
+      fs.renameSync(oldDataPath, dataPath);
+    }
   }
 
   corePath = path.join(dataPath, "core");
